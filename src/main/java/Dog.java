9da638aac690877d2e4d0a028772d017523d095e
yuvro <23ymultani@athenian.org>
2021-10-31 20:27:01 -0700

    Big rings just like a champion No TSA, hop on a jet, we gone

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,41 @@
 public class Dog extends Pet {
     // Instance variable(s)
+    private String breed;
+    public Dog(String name, int age, String breed){
+        super(name,age);
+        this.breed = breed;
 
+    }
+    public Dog(){
+        super("Fido",4);
+        this.breed = "Golden Retriever";
 
-    // Constructors
+    }
     
 
     // makeNoise() method
+    public void makeNoise(){
+        System.out.println("WOOF");
+    }
 
 
     // toString method
+    public String toString(){
+        String x = (getName() + ", a dog who is " + getAge() + "years old.");
+        return x;
+    }
 
 
     // Getter
+    public String getBreed(){
+        return this.breed;
+    }
+
 
 
     // Setter
+    public String setBreed(String b){
+        this.breed = b;
+        return this.breed;
+    }
 }
